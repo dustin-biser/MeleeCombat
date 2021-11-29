187182acc28d9c1f9e6da9643651ef35df5f9ab5
Dustin Biser <dustinbiser@gmail.com>
2021-11-29 12:39:06 -0800

    Init Repo
    Add gitignore

---
+++ Intermediate/Build/Win64/UE4Editor/Inc/MeleeCombatTutorial/MeleeCombatTutorial.init.gen.cpp
@@ -0,0 +1,35 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+#ifdef _MSC_VER
+#pragma warning (push)
+#pragma warning (disable : 4883)
+#endif
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeMeleeCombatTutorial_init() {}
+	UPackage* Z_Construct_UPackage__Script_MeleeCombatTutorial()
+	{
+		static UPackage* ReturnPackage = nullptr;
+		if (!ReturnPackage)
+		{
+			static const UE4CodeGen_Private::FPackageParams PackageParams = {
+				"/Script/MeleeCombatTutorial",
+				nullptr,
+				0,
+				PKG_CompiledIn | 0x00000000,
+				0x8C33CAA1,
+				0xF2E01B62,
+				METADATA_PARAMS(nullptr, 0)
+			};
+			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
+		}
+		return ReturnPackage;
+	}
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+#ifdef _MSC_VER
+#pragma warning (pop)
+#endif
---
+++ Intermediate/Build/Win64/UE4Editor/Inc/MeleeCombatTutorial/MeleeCombatTutorialGameModeBase.generated.h
@@ -0,0 +1,95 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef MELEECOMBATTUTORIAL_MeleeCombatTutorialGameModeBase_generated_h
+#error "MeleeCombatTutorialGameModeBase.generated.h already included, missing '#pragma once' in MeleeCombatTutorialGameModeBase.h"
+#endif
+#define MELEECOMBATTUTORIAL_MeleeCombatTutorialGameModeBase_generated_h
+
+#define MeleeCombatTutorial_Source_MeleeCombatTutorial_MeleeCombatTutorialGameModeBase_h_15_SPARSE_DATA
+#define MeleeCombatTutorial_Source_MeleeCombatTutorial_MeleeCombatTutorialGameModeBase_h_15_RPC_WRAPPERS
+#define MeleeCombatTutorial_Source_MeleeCombatTutorial_MeleeCombatTutorialGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define MeleeCombatTutorial_Source_MeleeCombatTutorial_MeleeCombatTutorialGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAMeleeCombatTutorialGameModeBase(); \
+	friend struct Z_Construct_UClass_AMeleeCombatTutorialGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AMeleeCombatTutorialGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/MeleeCombatTutorial"), NO_API) \
+	DECLARE_SERIALIZER(AMeleeCombatTutorialGameModeBase)
+
+
+#define MeleeCombatTutorial_Source_MeleeCombatTutorial_MeleeCombatTutorialGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesAMeleeCombatTutorialGameModeBase(); \
+	friend struct Z_Construct_UClass_AMeleeCombatTutorialGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AMeleeCombatTutorialGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/MeleeCombatTutorial"), NO_API) \
+	DECLARE_SERIALIZER(AMeleeCombatTutorialGameModeBase)
+
+
+#define MeleeCombatTutorial_Source_MeleeCombatTutorial_MeleeCombatTutorialGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AMeleeCombatTutorialGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AMeleeCombatTutorialGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AMeleeCombatTutorialGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AMeleeCombatTutorialGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AMeleeCombatTutorialGameModeBase(AMeleeCombatTutorialGameModeBase&&); \
+	NO_API AMeleeCombatTutorialGameModeBase(const AMeleeCombatTutorialGameModeBase&); \
+public:
+
+
+#define MeleeCombatTutorial_Source_MeleeCombatTutorial_MeleeCombatTutorialGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AMeleeCombatTutorialGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AMeleeCombatTutorialGameModeBase(AMeleeCombatTutorialGameModeBase&&); \
+	NO_API AMeleeCombatTutorialGameModeBase(const AMeleeCombatTutorialGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AMeleeCombatTutorialGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AMeleeCombatTutorialGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AMeleeCombatTutorialGameModeBase)
+
+
+#define MeleeCombatTutorial_Source_MeleeCombatTutorial_MeleeCombatTutorialGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define MeleeCombatTutorial_Source_MeleeCombatTutorial_MeleeCombatTutorialGameModeBase_h_12_PROLOG
+#define MeleeCombatTutorial_Source_MeleeCombatTutorial_MeleeCombatTutorialGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	MeleeCombatTutorial_Source_MeleeCombatTutorial_MeleeCombatTutorialGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	MeleeCombatTutorial_Source_MeleeCombatTutorial_MeleeCombatTutorialGameModeBase_h_15_SPARSE_DATA \
+	MeleeCombatTutorial_Source_MeleeCombatTutorial_MeleeCombatTutorialGameModeBase_h_15_RPC_WRAPPERS \
+	MeleeCombatTutorial_Source_MeleeCombatTutorial_MeleeCombatTutorialGameModeBase_h_15_INCLASS \
+	MeleeCombatTutorial_Source_MeleeCombatTutorial_MeleeCombatTutorialGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define MeleeCombatTutorial_Source_MeleeCombatTutorial_MeleeCombatTutorialGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	MeleeCombatTutorial_Source_MeleeCombatTutorial_MeleeCombatTutorialGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	MeleeCombatTutorial_Source_MeleeCombatTutorial_MeleeCombatTutorialGameModeBase_h_15_SPARSE_DATA \
+	MeleeCombatTutorial_Source_MeleeCombatTutorial_MeleeCombatTutorialGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	MeleeCombatTutorial_Source_MeleeCombatTutorial_MeleeCombatTutorialGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	MeleeCombatTutorial_Source_MeleeCombatTutorial_MeleeCombatTutorialGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> MELEECOMBATTUTORIAL_API UClass* StaticClass<class AMeleeCombatTutorialGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID MeleeCombatTutorial_Source_MeleeCombatTutorial_MeleeCombatTutorialGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
