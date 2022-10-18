24c021c675796d6992f96b474f664188344f7566
Dustin Biser <dustinbiser@gmail.com>
2022-10-17 17:58:55 -0700

    Saving updates over last few months
    10/17/2022 Updates:
    ) Add BP_Function "Pause Game for N Frames"
    ) Updated Draw Debug Spheres for Anim Notify Attack Active Section
    ) Anim Notify Attack Active Section now scales Damage Volume Spheres using Mesh Component Scale.
    ) Accidentally removed Ground plane, so added new one with updated NavMesh bounds.

--- Source/MeleeCombatTutorial/MeleeCombatTutorial.cpp
+++ Source/MeleeCombatTutorial/MeleeCombatTutorial.cpp
@@ -4,3 +4,14 @@
 #include "Modules/ModuleManager.h"
 
 IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MeleeCombatTutorial, "MeleeCombatTutorial" );
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Console Variables
+///////////////////////////////////////////////////////////////////////////////
+static TAutoConsoleVariable<bool> CVarDrawDamageVolumes(
+	TEXT("dev.DrawDamageVolumes"),
+	false,
+	TEXT("Toggle of drawing damage volumes associated with Anim Notify Active Sections.\n"),
+	ECVF_Scalability | ECVF_RenderThreadSafe);
+
