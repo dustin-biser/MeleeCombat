bac018bd553e3319254778184bb3d52b1e386bd7
Dustin Biser <dustinbiser@gmail.com>
2022-10-31 14:30:58 -0700

    Added Debug Overlays for "dev.DebugAimAssist" debug mode.
    + Add visibility trace to best targets for aim assist.

--- Source/MeleeCombatTutorial/MeleeCombatTutorial.cpp
+++ Source/MeleeCombatTutorial/MeleeCombatTutorial.cpp
@@ -9,9 +9,19 @@ IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MeleeCombatTutorial, "Mel
 ///////////////////////////////////////////////////////////////////////////////
 // Console Variables
 ///////////////////////////////////////////////////////////////////////////////
+
+/* Debug Draw Damage Volumes */
 static TAutoConsoleVariable<bool> CVarDrawDamageVolumes(
 	TEXT("dev.DrawDamageVolumes"),
 	false,
-	TEXT("Toggle of drawing damage volumes associated with Anim Notify Active Sections.\n"),
+	TEXT("Toggle of drawing damage volumes associated with Attacks, Projectiles, and Special Abilities.\n"),
+	ECVF_Scalability | ECVF_RenderThreadSafe);
+
+
+/* Debug Draw Aim Assist */
+static TAutoConsoleVariable<bool> CVarDebugAimAssist(
+	TEXT("dev.DebugAimAssist"),
+	false,
+	TEXT("Toggle debug mode for player Aim Assist.\n"),
 	ECVF_Scalability | ECVF_RenderThreadSafe);
 
